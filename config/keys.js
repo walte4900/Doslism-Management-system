09db7e3d3b5498999e2ed1e627ec4d926b767247
Sven Wang <doslism@outlook.com>
2021-01-19 10:08:50 +0800

    链接mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:1q2w3e4r@cluster0.qvnc0.mongodb.net/doslism01?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 /* Express建立服务器 */
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+// 引入数据库
+const db = require("./config/keys").mongoURI;
+// 链接数据库
+mongoose.connect(db).then(
+    ()=>console.log("database connected!")
+).catch(err=>console.log(err));
 
 app.get("/",(req,res)=>{
     res.send("Hello World!");
